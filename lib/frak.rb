a2207fe8ab27600d5c37cc1ca14096e3f093ada2
Agolveo <agolveo@gmail.com>
2014-05-18 21:58:41 -0700

    build search query uri, create history of queries

--- lib/frak.rb
+++ lib/frak.rb
@@ -1,14 +1,53 @@
-require "frak/version"
+require 'history'
 
-module Frak
+class Frak
 
-  class Search
-    # first   = ARGV[0]
-    # input   = ARGV - first 
+  def initialize(type, query)
+    @type = type
+    @query = query
 
-
-
-    
+    build_query
   end
 
+  def build_query 
+    @query = @query.join('+')
+
+    search_type
+  end
+
+  def search_type
+    case @type
+    when '-w'
+      @path = search_uri
+    when '-i'
+      @path = image_uri.join('&tbm=isch')
+      # https://www.google.com/search?q=ruby+gem&tbm=isch
+    when '-v'
+      @path = search_uri.join('&tbm=vid')
+      #q=ruby+gem&tbm=vid
+    when '-n'
+      @path = search_uri.join('&tbm=nws')
+      #q=ruby+gem&tbm=nws
+    end
+    return @path
+  end
+
+  def search_uri
+    Addressable::URI.parse('https://www.google.com/#q=').join(@query)
+  end
+
+  def image_uri
+    Addressable::URI.parse('https://www.google.com/search?q=').join(@query)
+  end
+
+  def write_history
+    History.new(@type, @query)
+  end
 end
+
+
+
+
+
+
+
